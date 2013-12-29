acc1948ab6b6b6ef2d9322bdae71613253fc1aef
Craig Condon <craig@spiceapps.com>
2013-12-29 13:03:45 -0800

    start on collection

---
+++ test/collection-test.js
@@ -0,0 +1,8 @@
+var bindable = require(".."),
+expect       = require("expect.js");
+
+describe("collection-test#", function () {
+  // - bind length
+  // - reset source
+  // - emit insert, remove, update, reset
+});
