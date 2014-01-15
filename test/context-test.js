d487c15b4440f6cdfe0d35882ca3831f2e347cb1
Craig Condon <craig@spiceapps.com>
2014-01-15 11:43:54 -0800

    added tests

---
+++ test/context-test.js
@@ -0,0 +1,37 @@
+var expect = require("expect.js"),
+bindable = require("..");
+
+
+describe("context-test#", function () {
+
+  describe("single property", function () {
+    it("can reference the binding when a function is called", function () {
+      var obj = new bindable.Object({ name: "a" });
+      obj.bind("name", function () {
+        expect(this.target).to.be(obj);
+      }).now();
+    });
+  });
+
+  describe("chained property", function () {
+    it("can reference the binding when a function is called", function () {
+      var obj = new bindable.Object({ "a.b.c.d": "e" });
+      obj.bind("a.b.c.d", function () {
+        expect(this.target).to.be(obj);
+      }).now();
+    });
+  });
+
+  describe("computed property", function () {
+    // TODO
+  })
+
+  describe("transformed listener", function () {
+    it("can reference the binding when a function is called", function () {
+      var obj = new bindable.Object({ "a.b.c.d": "e" });
+      obj.bind("a.b.c.d", { to: function () {
+        expect(this.target).to.be(obj);
+      }}).now();
+    })
+  });
+})
